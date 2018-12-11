a399165ee547e788edf157756c073ed250b732db
Ian Gornall <ian.gornall@gmail.com>
2018-12-10 19:54:37 -0500

    Updates

---
+++ precache-manifest.446f652aa03578ab7d01b8e919cf64af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9da920d018cf6cb30bf7354217a1a1",
+    "url": "/gemini-group/static/media/splash.bb9da920.jpg"
+  },
+  {
+    "revision": "36412c2a7539e5d9c6a9",
+    "url": "/gemini-group/static/js/runtime~main.36412c2a.js"
+  },
+  {
+    "revision": "fabce31a45ed5a288fb7",
+    "url": "/gemini-group/static/js/main.fabce31a.chunk.js"
+  },
+  {
+    "revision": "af571ed43fe5db2cca40",
+    "url": "/gemini-group/static/js/1.af571ed4.chunk.js"
+  },
+  {
+    "revision": "fabce31a45ed5a288fb7",
+    "url": "/gemini-group/static/css/main.6a043317.chunk.css"
+  },
+  {
+    "revision": "07e6633b066c57aafc5bfdfce36754a7",
+    "url": "/gemini-group/index.html"
+  }
+];
