1e1f7220631f3def61c8c03e52df9dbc9bb6f60d
Ian Gornall <ian.gornall@gmail.com>
2018-12-10 19:47:25 -0500

    Still working on deployment

---
+++ build/precache-manifest.2a1d33c86851d3c214ad2db596be69e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9da920d018cf6cb30bf7354217a1a1",
+    "url": "/gemini-group/build/static/media/splash.bb9da920.jpg"
+  },
+  {
+    "revision": "ac2eb6fdb03a52c5efab",
+    "url": "/gemini-group/build/static/js/runtime~main.ac2eb6fd.js"
+  },
+  {
+    "revision": "dcea79882ac0a89d8823",
+    "url": "/gemini-group/build/static/js/main.dcea7988.chunk.js"
+  },
+  {
+    "revision": "af571ed43fe5db2cca40",
+    "url": "/gemini-group/build/static/js/1.af571ed4.chunk.js"
+  },
+  {
+    "revision": "dcea79882ac0a89d8823",
+    "url": "/gemini-group/build/static/css/main.4ff5ebae.chunk.css"
+  },
+  {
+    "revision": "6fadf15aa834a563563408cfe0642021",
+    "url": "/gemini-group/build/index.html"
+  }
+];
