53c7c4e2b365d821598df6f01cf85909461b0f4e
Ian Gornall <ian.gornall@gmail.com>
2018-12-10 19:59:39 -0500

    Changed to hashrouter.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Header from './Header';
 import HomeScreen from './HomeScreen';
 import Footer from './Footer';
