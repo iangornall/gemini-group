fe13957c725cf2c5fe885fca3e656134a483c620
Ian Gornall <ian.gornall@gmail.com>
2018-12-10 19:31:47 -0500

    Added build files for github hosting.

---
+++ build/precache-manifest.bc7e137e34db04f35b5b314ae0e47e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9da920d018cf6cb30bf7354217a1a1",
+    "url": "/static/media/splash.bb9da920.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "645a7d816bfd7487eb4d",
+    "url": "/static/js/main.645a7d81.chunk.js"
+  },
+  {
+    "revision": "af571ed43fe5db2cca40",
+    "url": "/static/js/1.af571ed4.chunk.js"
+  },
+  {
+    "revision": "645a7d816bfd7487eb4d",
+    "url": "/static/css/main.a428d94d.chunk.css"
+  },
+  {
+    "revision": "df878c0d29def60046eb6c75f9ac0db7",
+    "url": "/index.html"
+  }
+];
