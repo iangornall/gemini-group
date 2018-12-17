aff3d62d7c609574b0e83d44906bdf247177f01f
Ian Gornall <ian.gornall@gmail.com>
2018-12-17 15:50:35 -0500

    refactored Splash component

--- src/shared/Splash.js
+++ src/shared/Splash.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import { withResizeDetector } from 'react-resize-detector';
 import Button from './Button';
 import './Splash.css';
-class Splash extends React.Component{
-  constructor(props){
-    super(props);
-    this.container = React.createRef();
-    this.updateContainer = this.updateContainer.bind(this);
-    this.state = {
-      width: 0,
-      x: 0,
-      y: 0
-    }
-  }
-  componentDidMount() {
-    this.updateContainer();
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if(this.props.width != prevProps.width){
-      this.updateContainer();
-    }
-  }
-  updateContainer() {
-    let containerRect = this.container.current.getBoundingClientRect();
-    this.setState({
-      width: containerRect.width,
-      x: containerRect.left,
-      y: containerRect.top
-    });
-  }
-  render(){
-    return (
-      <div ref={this.container} className="splash-container" style={{
-        backgroundImage: `url(assets/${this.props.background})`,
-        backgroundSize: this.state.width,
-        backgroundPosition: `${this.state.x}px ${this.state.y - (this.props.scrollY ? 32 : 0)}px`, 
-        minHeight: this.state.width / 3
-      }}>
-        <div className="splash-content" style={{opacity: 1 - this.props.scrollY / 200}}>
-          {this.props.title && <div className="splash-title" style={{marginTop: this.state.width / 3}}>{this.props.title}</div>}
-          {this.props.subtitle && <div><span className="splash-subtitle">{this.props.subtitle}</span></div>}
-          <div>
-            {this.props.buttons && this.props.buttons.map(button => <Button>{button}</Button>)}      
-          </div>
+let Splash = ({title, subtitle, buttons, background, scrollY,}) => (
+  <div className="splash-wrapper" style={{height: (title || subtitle || buttons) && 475 - scrollY}}>
+    <div className="splash-container" style={{
+      backgroundImage: `url(assets/${background})`,
+      height: 300 - scrollY
+    }} />
+    <div className="splash-content-wrapper">
+      <div className="splash-content" style={{opacity: 1 - scrollY / 200, marginTop: 335 - scrollY}}>
+        {title && <div className="splash-title">{title}</div>}
+        {subtitle && <div><span className="splash-subtitle">{subtitle}</span></div>}
+        <div className="splash-button-container">
+          {buttons && buttons.map(button => <Button>{button}</Button>)}      
         </div>
       </div>
-    )
-  }
-}
-export default withResizeDetector(Splash, {handleWidth: true, resizableElementId: 'root'});
+    </div>
+  </div>
+)
+export default Splash;
