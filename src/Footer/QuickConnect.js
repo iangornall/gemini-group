d4e56a3ec3380719bc2798effa9dea9c6e390f10
Ian Gornall <ian.gornall@gmail.com>
2018-12-20 14:39:38 -0500

    Connecting form to google sheets.

--- src/Footer/QuickConnect.js
+++ src/Footer/QuickConnect.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import Button from '../shared/Button';
-// let submitHandler = (event) =>{
-//   event.preventDefault();
-//   const data = new FormData(event.target);
-//   console.log("submit fired");
-//   var http = new XMLHttpRequest();
-//   var url = 'https://script.google.com/macros/s/AKfycbwTIRMnb6BXCjPepHShgEHB7j0mJ_GHiML1-Iz6KR_pCJQ1Nid4/exec';
-//   http.open('GET', url, true);
-
-//   //Send the proper header information along with the request
-//   http.setRequestHeader('Content-type', 'application/json; charset=utf-8"');
-
-//   http.onreadystatechange = function() {//Call a function when the state changes.
-//       if(http.readyState == 4 && http.status == 200) {
-//           alert(http.responseText);
-//       }
-//   }
-//   http.send(data);
-// }
+let submitHandler = (event) =>{
+  event.preventDefault();
+  console.log(event.currentTarget);
+  const data = new FormData(event.currentTarget);
+  console.log(data);
+  fetch('https://script.google.com/macros/s/AKfycbwTIRMnb6BXCjPepHShgEHB7j0mJ_GHiML1-Iz6KR_pCJQ1Nid4/exec', {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    // headers: {
+    //     "Content-Type": "application/json; charset=utf-8"
+    // },
+    body: data
+})
+.then(response => response.json())
+.then(data => console.log(data));
+}
+//action="https://formspree.io/matney@geminigroupatl.com" method="POST"
 let QuickConnect = () => (
   <div className="quick-connect">
     <h2 className="footer-title">Quick Connect</h2>
-    <form className="footer-form" action="https://formspree.io/matney@geminigroupatl.com" method="POST">
+    <form onSubmit={submitHandler} className="footer-form" >
       <label className="footer-label">First Name: 
         <input className="footer-input" type="text" name="first-name" placeholder="First Name" />
       </label>
