b68a92ab06d27b63964a70cdc21394fb5fa7dce5
Ian Gornall <ian.gornall@gmail.com>
2018-12-20 15:23:45 -0500

    Updates

---
+++ precache-manifest.d89e659f62ece626a3d3093d0cb5a086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e189f14d225630f5e67",
+    "url": "/static/js/main.3e189f14.chunk.js"
+  },
+  {
+    "revision": "aa87eecc71d32b11a2ad",
+    "url": "/static/js/1.aa87eecc.chunk.js"
+  },
+  {
+    "revision": "3e189f14d225630f5e67",
+    "url": "/static/css/main.7ed73b8b.chunk.css"
+  },
+  {
+    "revision": "aa87eecc71d32b11a2ad",
+    "url": "/static/css/1.7c3c7aec.chunk.css"
+  },
+  {
+    "revision": "64e7ef04f317823effad4bdedf08a819",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gemini-group/precache-manifest.df19d4255f7e3344c06db9bf0713b88e.js"
+  "/precache-manifest.d89e659f62ece626a3d3093d0cb5a086.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gemini-group/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
