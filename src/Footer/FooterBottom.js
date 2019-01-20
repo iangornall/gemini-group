0279e29af043e90cce69e7049ff90514e4a8a567
Ian Gornall <ian.gornall@gmail.com>
2019-01-20 14:43:35 -0500

    Updated leads upload to send one lead at a time.

--- src/Footer/FooterBottom.js
+++ src/Footer/FooterBottom.js
@@ -1,8 +1,8 @@
 import React from 'react';
 let FooterBottom = () => (
   <div className="footer-bottom">
-    <p>© 2018 Gemini Group.</p>
-    <p>Gemini Group and affiliated or subsidiary companies are not real estate brokers or agents. Gemini Group is a real estate investment company. All properties are either owned by Gemini Group or the company has a purchase contract and/or option with the owner of the property, which Gemini Group may assign to third parties. Gemini Group is not a real estate brokerage and does not provide realtor services to the public, or to any of the parties to which it has contractual relationships.</p>
+    <p>© 2018 Gemini Group ATL, LLC</p>
+    <p>Gemini Group ATL, LLC and affiliated or subsidiary companies are not real estate brokers or agents. Gemini Group ATL, LLC is a real estate investment company. All properties are either owned by Gemini Group ATL, LLC or the company has a purchase contract and/or option with the owner of the property, which Gemini Group ATL, LLC may assign to third parties. Gemini Group ATL, LLC is not a real estate brokerage and does not provide realtor services to the public, or to any of the parties to which it has contractual relationships.</p>
   </div>
 );
 export default FooterBottom;
--- src/LeadsScreen/LeadsForm.js
+++ src/LeadsScreen/LeadsForm.js
@@ -9,42 +9,117 @@ export default class LeadsForm extends React.Component{
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.sendToPodio = this.sendToPodio.bind(this);
+    this.createSlybroadcast = this.createSlybroadcast.bind(this);
+    this.triggerSlybroadcast = this.triggerSlybroadcast.bind(this);
     this.fileInput = React.createRef();
   }
   sendToPodio(json){
-    console.log(json);
-    fetch('https://secure.globiflow.com/catch/d7jiwnu6vssp1f0', 
+    return fetch('https://secure.globiflow.com/catch/5618hd399hkt2d8', 
     {
       method: "POST",
       mode: "cors",
       headers: {
           "Content-Type": "application/json",
-      },      
+      },
+      referrer: "no-referrer",
       body: json
-    })
-    // .then(response => response.json())
-    // .then(data => console.log(data));
+    });
   }
   CSVtoJSON(csv){
+    let insideQuotes = false;
+    let escaped = false;
+    let csvArr = csv.split('');
+    for (let i = 0; i < csvArr.length; i++){
+      if (!escaped && !insideQuotes && csvArr[i] === ','){
+        csvArr[i] = '|||';
+      }
+      if (!escaped && csvArr[i] === '"'){
+        insideQuotes = !insideQuotes;
+        csvArr[i] = "~~~"
+      }
+      if (csvArr[i] === '\\' && !escaped){
+        escaped = true;
+      } else {
+        escaped = false;
+      }
+    }
+    csv = csvArr.join('');
+    csv = csv.replace(/~~~/g, '');
     let lines = csv.split("\n");
-    let keys = lines[0].split(",");
+    let keys = lines[0].split("|||");
     let jsonData = [];
     for(let i = 1; i < lines.length; i++){
       let row = {};
-      let values = lines[i].split(",");
+      let values = lines[i].split("|||");
       for(let j = 0; j < keys.length; j++){
+        if (keys[j] === "Contact Phone"){
+          row.rawPhoneNumber = values[j];
+          let phoneNumberArr = values[j].split('');
+          phoneNumberArr = [...phoneNumberArr.slice(0,3), '-', ...phoneNumberArr.slice(3,6), '-', ...phoneNumberArr.slice(6)];
+          values[j] = phoneNumberArr.join('');
+        }
         row[keys[j]] = values[j];
       }
+      row.slybroadcast = this.state.slybroadcast;
       jsonData.push(row);
     }
-    return JSON.stringify(jsonData);
+    return jsonData;
+  }
+  createSlybroadcast(){
+    if (this.state.slybroadcast){
+      return fetch('https://secure.globiflow.com/catch/5s2h3vaq3g1k6j4',
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        referrer: "no-referrer",
+        body: `{"createSlybroadcast": "true"}`
+      });
+    } else {
+      return new Promise ((resolve) => resolve("No Slybroadcast"));
+    }
+  }
+  triggerSlybroadcast(){
+    if (this.state.slybroadcast){
+      return fetch('https://secure.globiflow.com/catch/ol4z6fs122y85cz',
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        referrer: "no-referrer",
+        body: `{"triggerSlybroadcast": "true"}`
+      });
+    }
+    else {
+      return new Promise ((resolve) => resolve("No Slybroadcast"));
+    }
   }
   handleSubmit(event) {
     event.preventDefault();
     let fr = new FileReader();
     fr.onload = (e) => {
-      let json = this.CSVtoJSON(e.target.result);
-      this.sendToPodio(json);
+      let jsonData = this.CSVtoJSON(e.target.result);
+      this.createSlybroadcast()
+      .then((result) => {
+        console.log("created slybroadcast", result);
+        (async () => {
+          for (let row of jsonData){
+            let response = (await this.sendToPodio(JSON.stringify(row)));
+            console.log("sent item", response);
+          }
+          // test
+          // let response = await this.sendToPodio(JSON.stringify(jsonData[1]));
+          // console.log("sent item", response);
+          // end test
+          let response2 = await this.triggerSlybroadcast()
+          console.log("triggered slybroadcast", response2);
+          console.log('all done');
+        })()
+      })
     };
     fr.readAsText(this.fileInput.current.files[0]);
   }
