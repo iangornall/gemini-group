addb7b9e318e8d6e684160fe3522e605643e7a20
Ian Gornall <ian.gornall@gmail.com>
2019-04-16 21:20:27 -0400

    added logos bbb and national prop team.

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -7,6 +7,8 @@ let Footer = () => (
   <footer className="footer">
     <div className="footer-columns">
       <FooterMenu />
+      <a href="https://www.bbb.org/us/ga/brookhaven/profile/real-estate-investing/gemini-group-atl-llc-0443-28052897" target="_blank"><img class="footer-img" src="./assets/bbb.png" alt="Better Business Bureau Accredited Business" /></a>
+      <a href="https://nationalpropertyteam.com/" target="_blank"><img class="footer-img" src="./assets/nationalPropertyTeam.jpg" alt="National Property Team" /></a>
     </div>
     <hr />
     <FooterBottom />
